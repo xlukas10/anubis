e80d73a50c8c2a5f3ac508b1c61a0f6364ad0dd9
xlukas10 <xlukas10@stud.feec.vutbr.cz>
2020-10-19 18:08:58 +0200

    Added some to-do functions description

--- anubis.py
+++ anubis.py
@@ -5,3 +5,72 @@ Created on Mon Oct 19 09:08:58 2020
 @author: Jakub Lukaszczyk
 """
 
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.config import Config
+
+#from harvesters import Harvester
+
+#Determine if there is a config file present and read it.
+#If the config file is not found a new one is made
+
+######Config.read('PATH TO .INI FILE')
+#set config
+######Config.write()
+
+
+#function to change tabs
+
+#classes defining individual tabs for kivy
+
+#function handeling file saving
+
+#function for import of an .xml config file
+
+#funcitoun for export of an .xml file
+
+#opening camera
+#   firewire
+#   gige
+#   usb
+
+#drawing camera image
+
+#play/pause
+
+#magnifying glass
+
+#Tensorflow
+
+#kivy UI
+
+#help - probably just open html help page
+
+#app settings save/load
+
+#maybe language settings
+
+
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='Password'))
+        self.password = TextInput(multiline=False,password=True)
+        self.add_widget(self.password)
+        self.add_widget(Button(text='Send'))
+
+
+class AnubisApp(App):
+    def build(self):
+        return LoginScreen()
+   
+    
+if __name__ == '__main__':
+    AnubisApp().run()
