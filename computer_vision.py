051a2dbd5c8d01c8d9b74cab53952c0b79e48353
xlukas10 <xlukas10@stud.feec.vutbr.cz>
2021-02-27 13:02:34 +0100

    Testing code for cat/dog unn

--- computer_vision.py
+++ computer_vision.py
@@ -7,10 +7,12 @@ Created on Sat Feb 27 09:00:50 2021
 
 from tensorflow import keras
 import numpy as np
+import cv2
 
 class Computer_vision():
     def __init__(self,plot):
-        pass
+        self.model = None
+        self.SIZE = 50
         
     def load_model(self,path):
         self.model = keras.models.load_model(path)
@@ -20,15 +22,18 @@ class Computer_vision():
     
     def classify(self,frame,prediction_flag):
         #get current frame
-        #frame_data = 
-        print(self.model.get_layer(index=0))
-        #maybe change dimensions according to model
-        #transform it to numpy array
-        
-        #run .predict
-        #self.model.predict(frame_data)
-        #save output to variable and write it to the plot
-        prediction_flag.clear()
+        if self.model != None:
+            frame_data = cv2.resize(frame,(self.SIZE,self.SIZE))
+            #maybe change dimensions according to model
+            frame_data = np.array(frame_data)
+            frame_data = frame_data.reshape(-1, self.SIZE, self.SIZE, 1)
+            #transform it to numpy array
+            
+            #run .predict
+            h = self.model.predict(frame_data)
+            print(h)
+            #save output to variable and write it to the plot
+            prediction_flag.clear()
         
     def learn(self):
         pass
