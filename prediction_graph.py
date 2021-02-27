e94ef11044f6e194d40aea3922d1a45b6e2b1c81
xlukas10 <xlukas10@stud.feec.vutbr.cz>
2021-02-27 20:15:47 +0100

    Prediction plot working. Still needs axes name and limits

--- prediction_graph.py
+++ prediction_graph.py
@@ -6,26 +6,36 @@ Created on Sat Feb 27 08:38:16 2021
 """
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg#, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Prediction_graph(FigureCanvasQTAgg):
     
-    def __init__(self, parent=None, width=5,heigth=4,dpi=100):
-        figure = Figure()
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        self.figure = Figure(figsize=(width, height), dpi=dpi)
         
-        self.axes = figure.add_axes([0,0,1,1])
+        self.axes = self.figure.add_subplot(111)
+        self.N = 0
         self.categories = []
         self.probability = []
+        self.w = 1
+        #self.indent = np.arange(self.N)
         
-        self.axes.bar(self.categories,self.probability)
-        #will probably need some resizing
-        super(Prediction_graph,self).__init__(figure)
+        self.axes.bar(self.categories,self.probability,self.w)
+        super(Prediction_graph,self).__init__(self.figure)
     
     def add_categories(self, categories = []):
-        self.categories = categories
-        self.axes.bar(self.categories,self.probability)
-        pass
+        self.categories.clear()
+        
+        for category in categories:
+            if(category != None):
+                self.categories.append(str(category))
     
     def write_probability(self, probability = []):
-        self.probability = probability
-        self.axes.bar(self.categories,self.probability)
-        pass
+        self.probability = probability[0]#test with multiple output classes
+        print(f"probab {self.probability}")
+        print(self.categories)
+        self.figure.canvas.axes.clear()
+        
+        self.axes.bar(self.categories,self.probability,self.w)
+        self.figure.canvas.draw()
