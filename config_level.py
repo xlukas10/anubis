3f634945cd6e62d341a421f39d978984588341c0
xlukas10 <xlukas10@stud.feec.vutbr.cz>
2021-03-04 12:04:47 +0100

    Added config level enum

---
+++ config_level.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 11:53:06 2021
+
+@author: Jakub Lukaszczyk
+"""
+import enum
+
+class Config_level(enum.IntEnum):
+    Unknown = 0
+    Beginner = 1
+    Expert = 2
+    Guru = 3
+    Invisible = 4
