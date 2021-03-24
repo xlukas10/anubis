454d697437f8a03764c6b61ea18eb6048a052fa8
xlukas10 <xlukas10@stud.feec.vutbr.cz>
2021-03-24 22:22:38 +0100

    Vendors enum instead of checking string

---
+++ vendors.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 19:53:55 2021
+
+@author: Jakub Lukaszczyk
+"""
+
+"""
+In this file there are defined all implemented vendors or more precisely their
+enum names
+"""
+import enum
+
+class Vendors(enum.IntEnum):
+    Other = 0
+    Allied_Vision_Technologies = 1
